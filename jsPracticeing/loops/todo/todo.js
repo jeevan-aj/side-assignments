8f4b3f967d6a4899fae672cf2a83fa7a5cf5eafe
jeevan-aj <ambrahamjeevan@gmail.com>
2023-10-17 18:54:56 +0530

    expense calculator done

--- jsPracticeing/loops/todo/todo.js
+++ jsPracticeing/loops/todo/todo.js
@@ -1,30 +1,29 @@
-let balance = document.getElementById("balance");
-let income = document.getElementById("incomediv_p");
+let balance = document.getElementById("balance-id");
+let income = document.getElementById("income-id");
+let expense = document.getElementById("expense_id");
 let item = document.getElementById("item-name");
 let cost = document.getElementById("item-cost");
 let enterButton = document.getElementById("enter_button_id");
 let transactionList = document.getElementById("transaction_list_ul");
 
-const dummyData = [
-    {id:1,discription:"jeevan", amount:-20},
-    {id:2,discription:"pum", amount:200},
-    {id:3,discription:"ambu", amount:300},
-    {id:4,discription:"kuttu", amount:120},
-]
+let totals ;
+let incomes;
+let expenses;
+const localStorageTrans = JSON.parse(localStorage.getItem("transactoin"));
+let transaction = localStorage.getItem("transaction") !== null ? localStorageTrans : [];
 
-let transaction = dummyData;
 
 function transactionDetails(transaction){
     // console.log(transaction);
     let sign = transaction.amount <0? "-":"+";
     const item = document.createElement("li");
-    item.innerHTML = `${transaction.discription};
+    item.innerHTML = `${transaction.discription}
     <span>${sign} ${Math.abs(transaction.amount)}</span>
-    <button class="remove-btn" onclick="removeTransaction(${transaction.id})">x</button>
+    <button class="removeBtn"  onclick="removeTransaction(${transaction.id})">x</button>
     `
     transactionList.appendChild(item);  
 }
-
+    
 //removeing item from list
 function removeTransaction(id){
     transaction = transaction.filter((e)=> e.id !=id);
@@ -39,18 +38,46 @@ function config(){
     amountUpdate(); 
 }
 function amountUpdate(){
-    const amounts =transaction.map((e)=>{
-    const total = amounts.reduce((acc,item)=> (acc+=item),0);
-    balance.innerHTML=`${total}`;
-    }  
-    );
+    const amounts =transaction.map((e)=>e.amount);
+    const total = amounts.reduce((acc,item)=> (acc += parseFloat(item)),1000);
+    balance.innerHTML=`${total}`; 
+    const incomeValue= amounts.filter((item)=> item> 0).reduce((acc,item) => (acc += parseFloat(item)),0);
+    income.innerHTML=incomeValue;
+    const expenseValue = amounts.filter((item)=> item<0).reduce((acc,item) => (acc -=item),0);
+    expense.innerHTML = `${expenseValue}`;
+    updateLocalStorage(); 
+    if(myChart){
+        updateChart(total,incomeValue,expenseValue);
+    }
+    else{
+        displayChart(total,incomeValue,expenseValue);
+    }
    
-   
+}
+//function to add value
+const localStoragetrans = JSON.parse
+function addTransaction(){
+    if(item.value == ""||cost.value==""){
+        alert("enter values first");
+    }
+    else{
+        curentTransaction = {
+            id : Math.floor(Math.random()*100000),
+            discription : item.value,
+            amount : cost.value
+        };
+        transaction.push(curentTransaction);
+        transactionDetails(curentTransaction);
+        amountUpdate();
+        item.value="";
+        cost.value = "";
+
+    }
 }
 
 window.addEventListener('load',function(){
     enterButton.addEventListener('click',function(){
-        myFunction();
+       addTransaction();
     });
     config();
 });
@@ -58,3 +85,29 @@ function myFunction(){
     let li = document.createElement("li");
     transactionList.appendChild(li);
 }
+
+function updateLocalStorage(){
+    localStorage.setItem("trans",JSON.stringify(transaction));
+}
+
+let myChart;
+function displayChart(total,income,expense){
+const ctx = document.getElementById('myChart').getContext("2d");
+  myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['total', 'income', 'expense'],
+      datasets: [{
+        label: '$',
+        data: [total,income,expense],
+        borderWidth: .1
+      }]
+    },
+  });
+}
+const updateChart = (total,income,expense)=> {
+    myChart.data.datasets[0].data[0] = total;
+    myChart.data.datasets[0].data[1] = income;
+    myChart.data.datasets[0].data[2] = expense;
+    myChart.update();
+  };
