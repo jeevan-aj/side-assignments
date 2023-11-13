5f97b64aaf2e2ec6ca11eb4789226b7c9c157566
jeevan-aj <ambrahamjeevan@gmail.com>
2023-11-13 20:23:53 +0530

    deleted unwanted

--- codewar/jsPracticeing/loops/assignments/sum_muloffive.js
+++
@@ -1,5 +0,0 @@
-const sumOfMultiples = (num)=> {
-    return Array.from({length:num},(_,index)=> index+1)
-    .filter((cur)=> cur%3==0||cur%5==0).reduce((acc,cur)=> acc+cur);
-}
-console.log(sumOfMultiples(10));
--- codewar/jsPracticeing/loops/assignments/sumof_even_below.js
+++
@@ -1,5 +0,0 @@
-const sumofEven = (num)=> {
-    return Array.from({length:num},(_,index)=> index+1)
-    .filter((cur)=> cur%2==0).reduce((acc,cur)=> acc+cur)
-}
-console.log(sumofEven(5));
