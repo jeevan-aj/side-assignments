ebd4bdffacbfd51a894efa30b6114b7ee6bd4b70
jeevan-aj <ambrahamjeevan@gmail.com>
2023-11-28 20:36:26 +0530

    codewars

--- codewar/sumoftwo.js
+++ codewar/sumoftwo.js
@@ -1,13 +1,12 @@
 var twoSum = function(nums, target) {
     let i,j;
     for(i=0;i<nums.length;i++){
-        for(j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                return [i,j]
-            }
+        let diff = target-nums[i]
+        if(nums.includes(diff)){
+            return [nums.indexOf(diff),i]
         }
-
     }
+    return "cannot find"
 };
 
-console.log(twoSum([2,7,11,15],9))
+console.log(twoSum([2,7,11,15],18))
