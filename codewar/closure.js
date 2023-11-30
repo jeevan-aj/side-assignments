fd17b0f456e8d014d9c271094633b8ecb07e07e8
jeevan-aj <ambrahamjeevan@gmail.com>
2023-11-30 20:33:42 +0530

    clouser

---
+++ codewar/closure.js
@@ -0,0 +1,33 @@
+// function parent(){
+//     let outerVar = 10;
+//     console.log(outerVar)
+//     function child(){
+//         console.log(outerVar)//accessing outerfunction variable
+//     }
+
+//     return child
+
+// }
+
+// // console.log(parent())
+
+// let newVar = parent()//assigning the return value of outer to newVar 
+
+// newVar()
+
+
+
+
+function parent(){
+    let a = 10;
+    console.log(a)
+    function kung(){
+        console.log(a)
+    }
+    return kung
+}
+let newValue = parent()
+newValue()
+
+
+
