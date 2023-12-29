8285614db7e2d8718e1996d6f4ee23622d353bd9
jeevan-aj <ambrahamjeevan@gmail.com>
2023-12-29 21:07:32 +0530

    duplicate count

---
+++ codewar/duplicate_count.js
@@ -0,0 +1,23 @@
+function duplicateCount(input) {
+    const charMap = {};
+    let count = 0;
+  
+    const lowerInput = input.toLowerCase();
+  
+    for (let i = 0; i < lowerInput.length; i++) {
+      const char = lowerInput[i];
+  
+      if (/^[a-z0-9]$/i.test(char)) {
+        if (charMap[char]) {
+          if (charMap[char] === 1) {
+            count++;
+          }
+          charMap[char]++;
+        } else {
+          charMap[char] = 1;
+        }
+      }
+    }
+  
+    return count;
+  }
